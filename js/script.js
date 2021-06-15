4ff2237758bcb3571e6ec000e8e8d702b6948cdf
Yalaes <yalaes.utrigas@gmail.com>
2021-06-15 11:28:31 +0700

    responsive done

--- js/script.js
+++ js/script.js
@@ -10,3 +10,22 @@ function toggleStuff () {
     burgerBtn.classList.toggle("open");
     navBurger.classList.toggle("toggle");
 }
+
+//fade in observer
+const toFade = document.querySelectorAll(".container");
+const options = {
+    root: null
+}
+
+const observer = new IntersectionObserver(beInTouch, options);
+toFade.forEach(el => {
+    observer.observe(el);
+})
+
+function beInTouch(entries) {
+    entries.forEach(element => {
+        if (element.isIntersecting) {
+            element.target.classList.add("fade")
+        }
+    });
+}
